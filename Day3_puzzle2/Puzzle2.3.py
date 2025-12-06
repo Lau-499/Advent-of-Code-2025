ab68c777773633a320c3f4675936e0fa45ea9cc6
Lau-499 <laurasanchezw@gmail.com>
2025-12-06 19:46:40 +0100

    Feat: Solution to the second puzzle of day 3

---
+++ Day3_puzzle2/Puzzle2.3.py
@@ -0,0 +1,46 @@
+f = open(r"Day3_puzzle1\batteries.txt")
+
+lines = f.readlines()
+
+sum = 0
+for line in lines:
+    listaS = list(line.strip("\n"))
+    nums = []
+    for i in range(len(listaS)):
+        nums.append(int(listaS[i]))
+    
+    print("Battery joltages:", nums)
+    result = []
+    k = 12
+    i = 0
+    n = len(nums)
+
+    while len(result) < k:
+        needed = k - len(result) # Still unselected numbers
+        remaining = n - i # Numbers left to consider
+        if remaining == needed:
+            result.extend(nums[i:]) # Must take all remaining numbers
+            break
+
+        end = n - needed
+        best_digit = 0
+        best_pos = 0
+
+        for pos in range(i, end + 1):
+            if nums[pos] > best_digit:
+                best_digit = nums[pos]
+                best_pos = pos
+        result.append(best_digit)
+        i = best_pos + 1
+    
+
+    
+    joltage = 0
+    for d in result:
+        joltage = joltage * 10 + d  
+    print("Largest joltage in this bank: ", joltage)
+    sum += joltage
+    print("Current total sum of largest joltage in all banks:", sum)
+
+
+
